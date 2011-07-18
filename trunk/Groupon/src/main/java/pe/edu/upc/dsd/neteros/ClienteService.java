6aeca72796dc6d57cfbdb6b815c37dd35b641801
carlospua@gmail.com <carlospua@gmail.com@a464c028-deb4-51f0-139a-81874ff4fc50>
2011-07-18 19:07:49 +0000

    cambio de proyecto

--- trunk/Groupon/src/main/java/pe/edu/upc/dsd/neteros/ClienteService.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.dsd.neteros;    
-import javax.jws.WebParam;
-import javax.jws.WebService;    
-
-@WebService  
-public interface ClienteService {            
-	public void registrar(@WebParam(name = "cliente") Cliente cliente); 
-	
-}
